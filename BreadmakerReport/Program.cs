fe53a01fddee441c858fd7efa76cf89eb4b1807a
Harpreet kaur <65912531+hudehyan@users.noreply.github.com>
2021-08-13 16:08:29 -0400

    Add files via upload

--- BreadmakerReport/Program.cs
+++ BreadmakerReport/Program.cs
@@ -15,17 +15,34 @@ namespace BreadmakerReport
         {
             Console.WriteLine("Welcome to Bread World");
             var BreadmakerDb = new BreadMakerSqliteContext(dbfile);
-            var BMList = BreadmakerDb.Breadmakers
-                // TODO: add LINQ logic ...
-                //       ...
+            var list = BreadmakerDb.Breadmakers
+                .Select(list_item => new
+                {
+                    list_detail = list_item.title,
+                    list_reviews = list_item.Reviews.Count(),
+                    list_avg = (Double)BreadmakerDb.Reviews
+                        .Where(i => i.BreadmakerId == list_item.BreadmakerId)
+                        .Select(i => i.stars).Sum() / list_item.Reviews.Count(),
+                })
                 .ToList();
 
-            Console.WriteLine("[#]  Reviews Average  Adjust    Description");
+            var BMList = list
+                .Select(list_item => new
+                {
+                    details = list_item.list_detail,
+                    reviews = list_item.list_reviews,
+                    avg = list_item.list_avg,
+                    adjust = ratingAdjustmentService.Adjust(list_item.list_avg, list_item.list_reviews)
+                })
+                .OrderByDescending(i => i.adjust)
+                .ToList();
+
+
+            Console.WriteLine("[#]  Reviews  Average  Adjust  Description");
             for (var j = 0; j < 3; j++)
             {
-                var i = BMList[j];
-                // TODO: add output
-                // Console.WriteLine( ... );
+                var row = BMList[j];
+                Console.WriteLine($"[{j + 1}]  {row.reviews,7}  {Math.Round(row.avg, 2),-7}  {Math.Round(row.adjust, 2),-6}   {row.details}");
             }
         }
     }
