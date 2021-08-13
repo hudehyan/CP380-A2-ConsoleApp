44e786ecb8d1f030b04574838ed76823d8f46033
Harpreet kaur <65912531+hudehyan@users.noreply.github.com>
2021-08-13 16:08:03 -0400

    Add files via upload

--- RatingAdjustmentService/RatingAdjustmentService.cs
+++ RatingAdjustmentService/RatingAdjustmentService.cs
@@ -2,11 +2,40 @@
 
 namespace RatingAdjustment.Services
 {
-    /** Service calculating a star rating accounting for the number of reviews
-     * 
-     */
+   
     public class RatingAdjustmentService
     {
-        // TODO: Replace this file with the one from Part A!
+        const double MAX_STARS = 5.0;  // Likert scale
+        const double Z = 1.96; // 95% confidence interval
+
+        double _q;
+        double _percent_positive;
+
+      
+        void SetPercentPositive(double stars)
+        {
+
+            _percent_positive = (stars * 20.0) / 100.0;
+
+        }
+
+      
+        void SetQ(double number_of_ratings)
+        {
+            _q = Z * Math.Sqrt(((_percent_positive * (1.0 - _percent_positive)) + ((Z * Z) / (4.0 * number_of_ratings))) / number_of_ratings);
+        }
+
+      
+        public double Adjust(double stars, double number_of_ratings)
+        {
+            if (stars <= MAX_STARS)
+            {
+                SetPercentPositive(stars);
+                SetQ(number_of_ratings);
+                double low = ((_percent_positive + ((Z * Z) / (2.0 * number_of_ratings)) - _q) / (1.0 + ((Z * Z) / number_of_ratings)));
+                return (low / 20.0) * 100.0;
+            }
+            return 0.0; 
+        }
     }
 }
